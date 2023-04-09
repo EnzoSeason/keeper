cc3780cba8c6baca20d305a2848d1635f025bfb8
Jijie LIU <liujijieseason@gmail.com>
2023-04-09 11:42:56 +0200

    Create IdentiftyServer

---
+++ src/MonoAPI/IdentityServer/Program.cs
@@ -0,0 +1,14 @@
+using IdentityServer;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder
+            .ConfigureServices()
+            .ConfigurePipeline();
+
+        app.Run();
+    }
+}
