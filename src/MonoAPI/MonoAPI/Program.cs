3871894fbeaac8c9bab96f05bcb03b979994ae93
Jijie LIU <liujijieseason@gmail.com>
2023-04-09 11:43:10 +0200

    cleanup MonoAPI

--- src/MonoAPI/MonoAPI/Program.cs
+++ src/MonoAPI/MonoAPI/Program.cs
@@ -1,32 +1,37 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MonoAPI.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-
-// TODO: remove after the real DAL is created
-builder.Services.AddDbContext<UserContext>(
-    opt => opt.UseInMemoryDatabase("User"));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args); 
+        
+        // Add services to the container.
+        builder.Services.AddControllers();
+
+        // TODO: remove after the real DAL is created
+        builder.Services.AddDbContext<UserContext>(
+            opt => opt.UseInMemoryDatabase("User"));
+
+       // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
 
