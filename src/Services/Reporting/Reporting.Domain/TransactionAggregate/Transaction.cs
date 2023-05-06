47128cdddf816592efed264552db3f873904c93e
Jijie LIU <liujijieseason@gmail.com>
2023-05-06 22:04:21 +0200

    [Reporting Domain] implement transaction validation

--- src/Services/Reporting/Reporting.Domain/TransactionAggregate/Transaction.cs
+++ src/Services/Reporting/Reporting.Domain/TransactionAggregate/Transaction.cs
@@ -1,12 +1,40 @@
+using System.ComponentModel.DataAnnotations;
 using Reporting.Domain.SeedWork;
 
 namespace Reporting.Domain.TransactionAggregate;
 
-public class Transaction: IAggregateRoot
+public class Transaction: IAggregateRoot, IValidatableObject
 {
-    public Guid UserId { get; init; }
+    public Guid ConfigId { get; init; }
     
     public DateTime FileDate { get; init; }
     
     public IEnumerable<TransactionRow> Rows { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var results = new List<ValidationResult>();
+
+        if (!Rows.Any())
+        {
+            results.Add(new ValidationResult("Rows must contain data.", new[] { nameof(Rows) }));
+        }
+
+        var rowsMonths = Rows.Select(row => row.Date.Month).Distinct();
+
+        if (rowsMonths.Count() != 1)
+        {
+            results.Add(
+                new ValidationResult("Rows must be the transactions of the same month.", new[] { nameof(Rows) }));
+        }
+
+        if (rowsMonths.First() != FileDate.Month)
+        {
+            results.Add(
+                new ValidationResult("The transaction date in rows doesn't match to the file date.",
+                    new[] { nameof(Rows) }));
+        }
+
+        return results;
+    }
 }
--- src/Services/Reporting/Reporting.Domain/TransactionAggregate/TransactionRow.cs
+++ src/Services/Reporting/Reporting.Domain/TransactionAggregate/TransactionRow.cs
@@ -1,10 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Reporting.Domain.TransactionAggregate;
 
-public class TransactionRow
+public class TransactionRow: IValidatableObject
 {
     public DateTime Date { get; set; }
     
     public string Label { get; set; }
     
-    public double amount { get; set; }
+    public double Amount { get; set; }
+    
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        var results = new List<ValidationResult>();
+
+        if (string.IsNullOrEmpty(Label))
+        {
+            results.Add(new ValidationResult("Label is required.", new[] { nameof(Label) }));
+        }
+
+        // Return the validation results
+        return results;
+    }
 }
