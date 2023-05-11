116ac82e010867bf2004e83a3bc2b2ac286474ee
Jijie LIU <liujijieseason@gmail.com>
2023-05-11 22:01:03 +0200

    [Reporting Domain] Save Transaction ConfigId as string in DB

--- src/Services/Reporting/Reporting.Domain/AggregatesModel/TransactionAggregate/Transaction.cs
+++ src/Services/Reporting/Reporting.Domain/AggregatesModel/TransactionAggregate/Transaction.cs
@@ -11,7 +11,7 @@ public record Transaction: IAggregateRoot, IValidatableObject
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
     
-    [BsonGuidRepresentation(GuidRepresentation.Standard)]
+    [BsonRepresentation(BsonType.String)]
     public Guid ConfigId { get; init; }
     
     public int Year { get; init; }
