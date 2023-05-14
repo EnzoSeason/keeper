1230491bd54240db2f9065d58a81d4096e324705
Jijie LIU <liujijieseason@gmail.com>
2023-05-14 17:31:25 +0200

    Update UploadTransactionFileHandlerTests.cs

--- src/Services/Reporting/Reporting.UTest/Commands/UploadTransactionFileHandlerTests.cs
+++ src/Services/Reporting/Reporting.UTest/Commands/UploadTransactionFileHandlerTests.cs
@@ -74,7 +74,7 @@ Date;Label;Amount;Currency;
                 {
                     Date = DateTime.Parse("2023-03-29"),
                     Label = "PYMT",
-                    Amount = decimal.Parse("16,03"),
+                    Amount = decimal.Parse("16,04"),
                     Currency = "EUR"
                 }
             }
