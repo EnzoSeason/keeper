a93fe6e301d1c9d0ff6d6ea9b2ebf5892d954b74
Jijie LIU <liujijieseason@gmail.com>
2023-05-21 18:32:47 +0200

    Create TransactionRowTests.cs

---
+++ src/Services/Reporting/Reporting.UTest/Domains/TransactionRowTests.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+using Reporting.Domain.AggregatesModel.ValueObjects;
+using Reporting.Domain.SeedWork;
+
+namespace Reporting.UTest.Domains;
+
+public class TransactionRowTests
+{
+    [Test]
+    public void Label_ShouldNotBeNullOrEmpty()
+    {
+        var row = new TransactionRow
+        {
+            Date = DateTime.UtcNow,
+            Amount = 1m,
+            Currency = "EUR"
+        };
+
+        Assert.That(DomainModelValidator<TransactionRow>.TryValidate(row, out var results), Is.False);
+        Assert.That(results.Count, Is.EqualTo(1));
+        Assert.That(results.First().ErrorMessage, Is.EqualTo("Label is required."));
+    }
+    
+    [Test]
+    public void Currency_ShouldNotBeNullOrEmpty()
+    {
+        var row = new TransactionRow
+        {
+            Label = "a",
+            Date = DateTime.UtcNow,
+            Amount = 1m,
+        };
+
+        Assert.That(DomainModelValidator<TransactionRow>.TryValidate(row, out var results), Is.False);
+        Assert.That(results.Count, Is.EqualTo(1));
+        Assert.That(results.First().ErrorMessage, Is.EqualTo("Currency is required."));
+    }
+
+    [Test]
+    public void Amount_ShouldNotBeZero()
+    {
+        var row = new TransactionRow
+        {
+            Label = "a",
+            Date = DateTime.UtcNow,
+            Amount = 0m,
+            Currency = "EUR"
+        };
+        
+        Assert.That(DomainModelValidator<TransactionRow>.TryValidate(row, out var results), Is.False);
+        Assert.That(results.Count, Is.EqualTo(1));
+        Assert.That(results.First().ErrorMessage, Is.EqualTo("Amount should not be 0."));
+    }
+}
