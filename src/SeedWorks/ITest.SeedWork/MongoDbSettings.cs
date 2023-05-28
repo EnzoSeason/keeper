316536e6dae45903112bb34eb788c5ba6435cd93
Jijie LIU <liujijieseason@gmail.com>
2023-05-28 22:19:27 +0200

    [Reporting Infra] fix CustomWebApplicationFactory

--- src/SeedWorks/ITest.SeedWork/MongoDbSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace ITest.SeedWork;
-
-public record MongoDbSettings
-{
-    public string ConnectionString { get; set; } = null!;
-
-    public string DatabaseName { get; set; } = null!;
-}
---
+++ src/Services/Reporting/Reporting.ITest/CustomWebApplicationFactory.cs
@@ -0,0 +1,24 @@
+using ITest.SeedWork;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Reporting.Infrastructure.Settings;
+
+namespace Reporting.ITest;
+
+public class CustomWebApplicationFactory<TProgram>
+    : WebApplicationFactory<TProgram> 
+    where TProgram : class
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureServices(services =>
+        {
+            services.Configure<MongoDbSettings>(settings =>
+            {
+                settings.ConnectionString = MongoDbTestInstance.ConnectionString;
+                settings.DatabaseName = MongoDbTestInstance.DatabaseName;
+            });
+        });
+    }
+}
